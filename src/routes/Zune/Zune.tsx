496d3b1cefa4818f204c5c7f68dc7a5cfa179461
Ervin Ding <ervin.ding@youi.tv>
2019-02-16 17:46:40 -0500

    conflicts

--- src/routes/Zune/Zune.tsx
+++ src/routes/Zune/Zune.tsx
@@ -40,6 +40,18 @@ const setupPlayer = (token:string):Spotify.SpotifyPlayer => {
     return player;
 }
 
+
+const triggerLogin = () => {
+  app.get('/login', function(req, res) {
+    var scopes = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize' +
+      '?response_type=code' +
+      '&client_id=' + my_client_id +
+      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+      '&redirect_uri=' + encodeURIComponent(redirect_uri));
+    });
+}
+
 class Zune extends Component {
 
     componentDidMount() {
