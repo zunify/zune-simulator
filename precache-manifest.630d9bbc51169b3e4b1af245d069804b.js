c95e9c6232a78af4a319d9af278ed2838d6eb845
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-17 02:14:42 -0500

    Updates

---
+++ precache-manifest.630d9bbc51169b3e4b1af245d069804b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d4b4596307dc3347b61",
+    "url": "/zune-simulator/static/js/runtime~main.0d4b4596.js"
+  },
+  {
+    "revision": "48a3547e4f95278688db",
+    "url": "/zune-simulator/static/js/main.48a3547e.chunk.js"
+  },
+  {
+    "revision": "d7805980bc943d5adc0a",
+    "url": "/zune-simulator/static/js/2.d7805980.chunk.js"
+  },
+  {
+    "revision": "48a3547e4f95278688db",
+    "url": "/zune-simulator/static/css/main.b9950ed4.chunk.css"
+  },
+  {
+    "revision": "d7805980bc943d5adc0a",
+    "url": "/zune-simulator/static/css/2.370cb552.chunk.css"
+  },
+  {
+    "revision": "04e758cc63c62554714d4d0fee5d0a02",
+    "url": "/zune-simulator/index.html"
+  }
+];
