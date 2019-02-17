b6794deaea9d7267fa6e8ecd72db61c707170b49
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-02-16 19:37:33 -0500

    ZUUUNE

--- src/routes/Zune/Zune.tsx
+++ src/routes/Zune/Zune.tsx
@@ -1,72 +1,80 @@
 import React, { Component } from 'react';
 
+import { Button } from 'antd';
 
+import 'antd/dist/antd.css';
 import './Zune.css'
 
-
 let player:Spotify.SpotifyPlayer;
+const token = 'BQAnRNM0LPlLGHnlhSvk9FWc1gS1qRILDLJG0XsJ1BJTA_v7Wt5LHZ8Shm5vPkj2RUN4g7JtSlEsAx8PChM6G6-ciCVrWJU5_l0EypblmaGs4WUGKq2ByRpp7dyNAu2c5qYZlglgbxCjKIi0R2baT7z5_gB4t58VpNOoOyA';
+
+const setupPlayer = (token:string):Spotify.SpotifyPlayer => { 
+    window.onSpotifyWebPlaybackSDKReady = () => {
+            player = new Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(token); }
+        });
+
+        // Error handling
+        player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        player.addListener('account_error', ({ message }) => { console.error(message); });
+        player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+        // Playback status updates
+        player.addListener('player_state_changed', state => { console.log(state); });
+
+        // Ready
+        player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+        });
+
+        // Not Ready
+        player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+        });
+
+        // Connect to the player!
+        player.connect(); 
+    };
+    return player;
+}
 
 class Zune extends Component {
 
     componentDidMount() {
-        this.setupPlayer()
+        //setupPlayer(token)
     }
 
     render() {
         return (
             <div className="Zune">
-                <div className='border'>
-                    <div className='screen'>
-                        screen
-                    </div>
-                </div>
-                <div className='controls'>
-                    <div className='control back'></div>
-                    <div className='control wheel'></div>
-                    <div className='control toggle'></div>
-                </div>
                 <button type='button' onClick={()=>{this.startPlayback(player)}}>START</button>
                 <button type='button' onClick={()=>{this.togglePlayer(player)}}>PLAY/PAUSE</button>
                 <button type='button' onClick={()=>{this.nextTrack(player)}}>NEXT</button>
-                <button type='button' onClick={()=>{this.prevTrack(player)}}>PREVIOUS</button>            
+                <button type='button' onClick={()=>{this.prevTrack(player)}}>PREVIOUS</button>      
+                <div className='border'>
+                    <div className='screen'>
+                        <div style={{width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                            <img height='200' src='./zune.svg' />
+                        </div>
+                    </div>
+                </div>
+                <Button onClick={()=>{console.log('asd')}} type="primary">Primary</Button>
+
+                <div className='controls'>
+                    <div className='control back'><i className="fas fa-arrow-left"></i></div>
+                    <div className='control wheel'></div>
+
+                    <button type='button' onClick={()=>{console.log('asd')}} className='control toggle'><i className="fas fa-play"></i></button>
+                </div>
+                {player && <script src="https://sdk.scdn.co/spotify-player.js"></script>}
             </div>
         );
     }
 
-    private setupPlayer = () => { 
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            const token = 'BQAFqwRFc-F6WEkJA-R_SdxJ6CLiKSYQWbhtOOBUQUT3eayzFCtg8MbxEtlUS7AtLsmsxRBB4eOJd-mDaDAZJrgSZxpVBYhjwKlWGpOy8I1N9Y8VTy2YJCrlN0gKZg1x8SLSKqaGjhfnpUkdY0ip0VpBkukw-k_mLpC0XsY';
-                player = new Spotify.Player({
-                name: 'Web Playback SDK Quick Start Player',
-                getOAuthToken: cb => { cb(token); }
-            });
-
-            // Error handling
-            player.addListener('initialization_error', ({ message }) => { console.error(message); });
-            player.addListener('authentication_error', ({ message }) => { console.error(message); });
-            player.addListener('account_error', ({ message }) => { console.error(message); });
-            player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-            // Playback status updates
-            player.addListener('player_state_changed', state => { console.log(state); });
-
-            // Ready
-            player.addListener('ready', ({ device_id }) => {
-            console.log('Ready with Device ID', device_id);
-            });
-
-            // Not Ready
-            player.addListener('not_ready', ({ device_id }) => {
-            console.log('Device ID has gone offline', device_id);
-            });
-
-            // Connect to the player!
-            player.connect();
-            return player
-        };
-    }
-
     private startPlayback = (player: Spotify.SpotifyPlayer) => { 
+        console.log('asd')
         player.connect();
       }
       
